f465497435628b6fc2c1f7148cd475ec3888d369
Leonid Titov <ltmolprod@gmail.com>
2021-06-01 09:45:59 +0300

    20210601-094559 {50acf84d47cd5f39967689b4b4b30a69d18a328b}

--- filtertag.go
+++ filtertag.go
@@ -1,3 +1,341 @@
 package filtertag
 
-import ()
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	deep_copy "github.com/mitchellh/copystructure"
+)
+
+type Logger struct {
+	Output           io.Writer
+	ActiveFiltertags map[string]bool
+	ExitFunc         func(int)
+}
+
+type Entry struct {
+	Fields    map[string]interface{}
+	Filtertag string
+	LoggerCh  chan *LoggerChType
+	ChDown    chan *LoggerChType
+
+	prev_entry_filtertag string
+	rawline              []byte
+}
+
+type LoggerChType struct {
+	Command       int
+	Logger        *Logger
+	CookedLogLine *CookedLogLine
+	ChDown        chan *LoggerChType
+}
+
+type CookedLogLine struct {
+	Filtertag string
+	RawLine   []byte
+}
+
+const (
+	Cmd_WriteLine int = iota
+	Cmd_GetLogger
+	Cmd_SetLogger
+	Cmd_FatalExit
+)
+
+func MakePrimordialEntryWithLogger(ctx context.Context) (entry *Entry) {
+	var err error
+
+	logger := &Logger{
+		// these are defaults, you can change these by API
+		Output: os.Stderr,
+		ActiveFiltertags: map[string]bool{
+			"info":  true,
+			"error": true,
+			"fatal": true,
+			"panic": true,
+		},
+		ExitFunc: os.Exit,
+	}
+
+	ch_i1 := make(chan *LoggerChType, 500)
+	host, err := os.Hostname()
+	if err != nil {
+		panic(fmt.Errorf("!!! filtertag.go:66 / *** at \"host, err	:= os.Hostname()\": %v", err))
+	}
+	executable, err := os.Executable()
+	if err != nil {
+		panic(fmt.Errorf("!!! filtertag.go:67 / *** at \"executable, err	:= os.Executable()\": %v", err))
+	}
+
+	entry = &Entry{
+		Fields: map[string]interface{}{
+			"timestamp":  "",
+			"host":       host,
+			"service":    executable,
+			"subsystem":  "",
+			"ctxpretext": "",
+			"err":        "",
+			"msg":        "",
+		},
+		Filtertag: "info",
+		LoggerCh:  ch_i1,
+	}
+
+	go func() {
+		var msg *LoggerChType
+		for {
+			select {
+			case msg = <-ch_i1:
+				switch msg.Command {
+				case Cmd_WriteLine:
+					if v, ok := logger.ActiveFiltertags[msg.CookedLogLine.Filtertag]; ok && v == true {
+						_, err = logger.Output.Write(msg.CookedLogLine.RawLine)
+						if err != nil {
+							panic(fmt.Errorf("!!! filtertag.go:91 / *** at \"_, err = logger.Output.Write( msg.CookedLogLine.RawLine )\": %v", err))
+						}
+					}
+				case Cmd_GetLogger:
+					// we can't return the original, because a user may start touching it, and it'll race-condition-crash the program
+					logger2, err := deep_copy.Copy(logger)
+					if err != nil {
+						continue
+					}
+					func() {
+						defer func() {
+							if errrec := recover(); errrec != nil {
+								err = fmt.Errorf("try{} panicked at \"msg.Logger = logger2.(*Logger)\": %v", errrec)
+							}
+						}()
+						err = nil
+						msg.Logger = logger2.(*Logger)
+					}()
+					if err != nil {
+						continue
+					}
+					select {
+					case msg.ChDown <- msg:
+					default:
+					}
+				case Cmd_SetLogger:
+					// we can't set the original, because a user may start touching it, and it'll race-condition-crash the program
+					logger2, _ := deep_copy.Copy(msg.Logger)
+					if err != nil {
+						continue
+					}
+					func() {
+						defer func() {
+							if errrec := recover(); errrec != nil {
+								err = fmt.Errorf("try{} panicked at \"logger = logger2.(*Logger)\": %v", errrec)
+							}
+						}()
+						err = nil
+						logger = logger2.(*Logger)
+					}()
+				case Cmd_FatalExit:
+					logger.ExitFunc(1)
+				}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	return entry
+}
+
+// DO NOT run Get/Set logger concurrently! Only one thread is allowed to run them
+func (entry *Entry) GetLogger() (logger *Logger) {
+
+	if entry.ChDown == nil {
+		entry.ChDown = make(chan *LoggerChType, 1)
+	}
+
+	msg := &LoggerChType{Command: Cmd_GetLogger, Logger: logger, ChDown: entry.ChDown}
+
+	entry.LoggerCh <- msg
+
+	select {
+	case <-time.After(2000 * time.Millisecond):
+		panic(fmt.Errorf(", \"msg = <-entry.ChDown @!! 200\""))
+	case msg = <-entry.ChDown:
+	}
+
+	return msg.Logger
+}
+
+func (entry *Entry) SetLogger(logger *Logger) (err error) {
+
+	msg := &LoggerChType{Command: Cmd_SetLogger, Logger: logger}
+
+	entry.LoggerCh <- msg
+
+	return
+}
+
+func (entry *Entry) Writer() (w io.Writer) {
+	w = &Writer{
+		Entry: entry,
+	}
+	return w
+}
+
+type Writer struct {
+	Entry *Entry
+}
+
+func (w *Writer) Write(p []byte) (n int, err error) {
+	w.Entry.Log(string(p))
+	n = len(p)
+	return
+}
+
+// Normal, base logging func
+func (entry *Entry) Logft(
+	filtertag string,
+	formatstring string,
+	args ...interface{},
+) {
+	var err error
+
+	if ft, ok := entry.Fields["filtertag"]; ok {
+		entry.prev_entry_filtertag = ft.(string)
+	} else {
+		entry.prev_entry_filtertag = ""
+	}
+
+	entry.Fields["msg"] = fmt.Sprintf(formatstring, args...)
+
+	entry.Fields["timestamp"] = time.Now().Format("2006-01-02 15:04:05.000 MST")
+
+	entry.rawline, err = json.Marshal(entry.Fields)
+	if err != nil {
+		panic(fmt.Errorf("!!! filtertag.go:194 / *** at \"entry.rawline, err = json.Marshal( entry.Fields)\": %v", err))
+	}
+
+	msg := &LoggerChType{
+		Command: Cmd_WriteLine,
+		CookedLogLine: &CookedLogLine{
+			Filtertag: filtertag,
+			RawLine:   entry.rawline,
+		},
+	}
+
+	entry.LoggerCh <- msg
+
+	entry.Fields["filtertag"] = entry.prev_entry_filtertag
+	entry.Fields["err"] = ""
+	entry.Fields["msg"] = ""
+}
+
+// Normal, shorter, filtertag is inferred from the Entry
+func (entry *Entry) Log(
+	formatstring string,
+	args ...interface{},
+) {
+	entry.Logft(entry.Filtertag, formatstring, args)
+}
+
+// Filtertag="fatal", and as a special case it commands to exit the program (via ExitFunc / os.Exit())
+func (entry *Entry) Fatal(
+	formatstring string,
+	args ...interface{},
+) {
+	entry.Logft("fatal", formatstring, args)
+	entry.LoggerCh <- &LoggerChType{Command: Cmd_FatalExit}
+}
+
+// Filtertag="panic", and as a special case it panics
+func (entry *Entry) Panic(
+	formatstring string,
+	args ...interface{},
+) {
+	entry.Logft("panic", formatstring, args)
+	err := fmt.Errorf(formatstring, args...)
+	panic(err)
+}
+
+// A synonymical funcs follow
+func (entry *Entry) Trace(
+	formatstring string,
+	args ...interface{},
+) {
+	entry.Logft("trace", formatstring, args)
+}
+
+func (entry *Entry) Debug(
+	formatstring string,
+	args ...interface{},
+) {
+	entry.Logft("debug", formatstring, args)
+}
+
+func (entry *Entry) Info(
+	formatstring string,
+	args ...interface{},
+) {
+	entry.Logft("info", formatstring, args)
+}
+
+func (entry *Entry) Warning(
+	formatstring string,
+	args ...interface{},
+) {
+	entry.Logft("warning", formatstring, args)
+}
+
+func (entry *Entry) Error(
+	formatstring string,
+	args ...interface{},
+) {
+	entry.Logft("error", formatstring, args)
+}
+
+func (entry *Entry) Emergency(
+	formatstring string,
+	args ...interface{},
+) {
+	entry.Logft("emergency", formatstring, args)
+}
+
+func (entry *Entry) Alert(
+	formatstring string,
+	args ...interface{},
+) {
+	entry.Logft("alert", formatstring, args)
+}
+
+func (entry *Entry) Critical(
+	formatstring string,
+	args ...interface{},
+) {
+	entry.Logft("critical", formatstring, args)
+}
+
+func (entry *Entry) Warn(
+	formatstring string,
+	args ...interface{},
+) {
+	entry.Logft("warn", formatstring, args)
+}
+
+func (entry *Entry) Notice(
+	formatstring string,
+	args ...interface{},
+) {
+	entry.Logft("notice", formatstring, args)
+}
+
+func (entry *Entry) Informational(
+	formatstring string,
+	args ...interface{},
+) {
+	entry.Logft("informational", formatstring, args)
+}
+
+func Hello() {
+	fmt.Println("HELLO FILTERTAG")
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/rusriver/filtertag
 
 go 1.16
+
+require github.com/mitchellh/copystructure v1.2.0
